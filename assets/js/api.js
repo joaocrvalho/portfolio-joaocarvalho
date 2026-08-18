4f49959bd058ac3e6c11fb1cd34373beff264ab6
joaocrvalho <jvcoliveira2006@.gmail.com>
2026-08-18 16:26:18 -0300

    adiocionando os icones das tecnologias

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData(){
-    const url = "https://raw.githubusercontent.com/joaocrvalho/js-developer-portfolio/refs/heads/main/data/profile.json";
+    const url = "https://raw.githubusercontent.com/joaocrvalho/portfolio-joaocarvalho/refs/heads/main/data/profile.json";
     const fetching = await fetch(url);
     return await fetching.json();
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,36 @@
-(async () => {
+function updateProfileInfo(profileData){
+    const photo = document.getElementById("profile.photo");
+    photo.src = profileData.photo;
+    photo.alt = profileData.name;
 
+    const name = document.getElementById("profile.name");
+    name.innerText = profileData.name;
+
+    const job = document.getElementById("profile.job");
+    job.innerText = profileData.job;
+
+    const location = document.getElementById("profile.location");
+    location.innerText = profileData.location;
+
+    const phone = document.getElementById("profile.phone");
+    phone.innerText = profileData.phone;
+    phone.href = `tel:${profileData.phone}`;
+
+    const email = document.getElementById("profile.email");
+    email.innerHTML = profileData.email;
+    email.href = `malito:${profileData.email}`;
+}
+
+function updateSoftSkills(profileData){
+    const softSkills = document.getElementById("softSkills");
+
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('');
+}
+
+
+
+(async () => {
     const profileData = await fetchProfileData();
-    console.log(profileData);
+    updateProfileInfo(profileData);
+    updateSoftSkills(profileData)
 })()
